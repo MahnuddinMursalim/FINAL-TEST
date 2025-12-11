28474d2d476ac4105d818cee1af6b35440cf13e6
Mahmuddin Mursalim <mahmuddinmursalim5@gmail.com>
2025-12-11 16:08:25 +0800

    Add files via upload

---
+++ Array dimensi 1.py
@@ -0,0 +1,10 @@
+#Nama	: Mahmuddin Mursalim
+#Nim	: D0425509
+#Kelas	: B Sistem Informasi
+
+# Array 1 Dimensi
+#Definisi Array 1 dimensi adalah struktur data yang berisi kumpulan elemen dalam satu baris secara linear.
+array1 = [100, 200, 300, 400, 500]
+
+print("Array 1D:", array1)
+print("Elemen pertama:", array1[0])
