31a3b05d9826ca12afc3d7e7929ffd6559b8d134
Mahmuddin Mursalim <mahmuddinmursalim5@gmail.com>
2025-12-11 17:26:47 +0800

    Delete Tipe data utama/Tipe data string.py

--- Tipe data utama/Tipe data string.py
+++
@@ -1,8 +0,0 @@
-#Nama	: Mahmuddin Mursalim
-#Nim	: D0425509
-#Kelas	: B Sistem Informasi
-
-#String adalah tipe data yang berisi teks atau kumpulan karakter yang berada dalam tanda kutip.
-
-nama = "Mahmuddin_Mursalim"
-print(type(nama))   # Output: <class 'str'>
